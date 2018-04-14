b849fe0c7f482eca08e0da7c1b23971130d958e9
per.griffiths <per.griffiths@jpmorgan.com>
2018-04-14 17:36:20 +0800

    openfin play

---
+++ javascript/openfin-amps-demo/gulp/tasks/openfin.js
@@ -0,0 +1,12 @@
+var gulp   = require('gulp');
+var src  = require('../config').js;
+
+var openfinLauncher = require('openfin-launcher'),
+    path = require('path');
+ 
+gulp.task('openfin', function() {
+  openfinLauncher.launchOpenFin({
+      //configPath: 'file:/' + path.resolve('app.json')
+      configPath: 'C:/Users/Per/Source/Repos/sandbox/javascript/openfin-amps-demo/app.json'
+  });
+});
