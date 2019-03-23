05738691ae683c42b55efa88b36ca17288d2bb8a
pergamonster <pergriffiths@gmail.com>
2019-03-23 13:50:04 +0800

    regex postal code

--- python/hackerrank.py
+++ python/hackerrank.py
@@ -4,3 +4,13 @@ K,M = map(int,input().split())
 N = (list(map(int, input().split()))[1:] for _ in range(K))
 results = map(lambda x: sum(i**2 for i in x)%M, product(*N))
 print(max(results))
+
+regex_integer_in_range = r"^[1-9][\d]{5}$"	# Do not delete 'r'.
+regex_alternating_repetitive_digit_pair = r"(\d)(?=\d\1)"	# Do not delete 'r'.
+
+
+import re
+P = input()
+
+print (bool(re.match(regex_integer_in_range, P)) 
+and len(re.findall(regex_alternating_repetitive_digit_pair, P)) < 2)
